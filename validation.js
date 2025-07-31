0b7f358aa1ed09167c9603b1eae2351036228e03
Devon  Ivory <devonivory@Devons-MacBook-Air.local>
2025-07-30 21:21:47 -0400

    added the logic to change the action attribute of form if login or register is clicked

--- validation.js
+++ validation.js
@@ -18,14 +18,16 @@ window.onload = function() {
         window.history.replaceState({}, document.title, window.location.pathname);
     }
 }
-function validateForm(){
+function validateForm(event){
     let password = document.registrationForm.Player.value;
     let player = document.registrationForm.password.value;
+    let submitter = event.submitter;
     let isValid = true
+   
     printError("passwordHeader", "Password");
     printError("playerHeader","Player Name");
    
-    console.log(`${player} player :   Password  ${password}`);
+    console.log(`${player} player :   EVENT  ${submitter.value}`);
     if(password == ""){
         // printError("passwordHeader","please don't leave password blank");
         alert("Please don't leave password field blank");
@@ -42,9 +44,21 @@ function validateForm(){
         
             printError("passwordHeader", "Password");
             printError("playerHeader","Player Name");
+            if(submitter.value == "login"){
+               
+                document.registrationForm.action ="loginForm.php";
+                
+        
+            }
+            else if (submitter.value == "register"){
+                    
+                    document.registrationForm.action = "registerForm.php";
+                    
+            }
 
         
     }
+   
     return isValid;
 }function printError(elemId, hintMsg) {
     document.getElementById(elemId).innerHTML = hintMsg;
